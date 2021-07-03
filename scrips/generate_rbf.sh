abf1dbf8fbce46d7c78e5f41010edeb0cba8b861
Nicolas Schuck <nico-schuck@online.de>
2021-07-03 19:54:11 +0200

    working kernel with buildroot_18 root file system seems not to be working...

--- scrips/generate_rbf.sh
+++ scrips/generate_rbf.sh
@@ -7,11 +7,10 @@ quartus_cpf -c -o bitstream_compression=on De1-SOC.sof De1-SOC.rbf
 echo "--done--"
 
 
-echo "generate dts"
+echo "generate dtb"
 export PATH=$PATH:~/intelFPGA/20.1/embedded/
 ./embedded_command_shell.sh
-sopc2dts --input soc_system.sopcinfo\
---output De1-SOC.dtb --type dtb\
---board soc_system_board_info.xml\
---board hps_common_board_info.xml\
---clocks
+sopc2dts --input DE1_SoC.sopcinfo --output DE1_SOC.dtb --type dtb --board soc_system_board_info.xml --board hps_common_board_info.xml --clocks
+
+"generate dts"
+sopc2dts --input DE1_SoC.sopcinfo --output DE1_SOC.dts --type dts --board soc_system_board_info.xml --board hps_common_board_info.xml --clocks
